1aefb11918f30edbb659b28307974ae57ed7b11a
Actex12 <124183879+Actex12@users.noreply.github.com>
2023-02-02 16:57:36 -0500

    Rename dbo.UpdateSequence.StoredProcedure.sql to SQL/dbo.UpdateSequence.StoredProcedure.sql

---
+++ SQL/dbo.UpdateSequence.StoredProcedure.sql
@@ -0,0 +1,24 @@
+﻿USE [PS]
+GO
+/****** Object:  StoredProcedure [dbo].[UpdateSequence]    Script Date: 2/1/2023 12:14:48 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[UpdateSequence]
+as
+-- this procedure updates the sequence number
+
+  DECLARE @int int
+  SET @int = 0
+  UPDATE dbo.PokerMaster
+  SET SequenceNumber = @int,
+	  @int = @int +1
+FROM dbo.PokerMaster
+
+
+
+
+GO
