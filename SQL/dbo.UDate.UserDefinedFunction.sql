1928af0dd779ffb3dc101f28b729a69a746f534b
Actex12 <124183879+Actex12@users.noreply.github.com>
2023-02-02 16:56:44 -0500

    Rename dbo.UDate.UserDefinedFunction.sql to SQL/dbo.UDate.UserDefinedFunction.sql

---
+++ SQL/dbo.UDate.UserDefinedFunction.sql
@@ -0,0 +1,30 @@
+﻿USE [PS]
+GO
+/****** Object:  UserDefinedFunction [dbo].[UDate]    Script Date: 2/1/2023 12:14:48 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[UDate] (@CLM varchar(3))
+RETURNS INT
+AS
+BEGIN
+DECLARE @A NVARCHAR(300)
+SET @A = '
+UPDATE dbo.Tally
+SET '+@CLM+'_Comit = b.Comit,
+	'+@CLM+'_Rse = b.Rse,
+	'+@CLM+'_AllIn = b.AllIN,
+	'+@CLM+'_Fold = b.Fold,
+	'+@CLM+'_Award = b.Award,
+	'+@CLM+'_Uncalled = b.Uncalled
+FROM dbo.Tally a INNER JOIN dbo.TallyTemp b on a.HandID = b.HandID and a.TourneyID = b.TourneyID and
+a.Seat = b.Seat'
+
+exec sp_executesql @A
+
+RETURN @@rowcount
+
+END
+GO
