cf8fdfca72a42921f43511779fe7d18d367a4d34
Actex12 <124183879+Actex12@users.noreply.github.com>
2023-02-02 16:53:00 -0500

    Rename dbo.ChkKwds.StoredProcedure.sql to SQL/dbo.ChkKwds.StoredProcedure.sql

---
+++ SQL/dbo.ChkKwds.StoredProcedure.sql
@@ -0,0 +1,12 @@
+﻿USE [PS]
+GO
+/****** Object:  StoredProcedure [dbo].[ChkKwds]    Script Date: 2/1/2023 12:14:48 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[ChkKwds]
+as
+
+-- this is a placeholder to check for keywords in player names
+GO
